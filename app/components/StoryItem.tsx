0e2e5de261ec2a0d72ca7ac8057d8913920bbfac
Shashikant <kant707@gmail.com>
2024-09-13 02:34:13 +0530

    hardcoded user name add to profile pic

--- app/components/StoryItem.tsx
+++ app/components/StoryItem.tsx
@@ -5,8 +5,11 @@ interface StoryItemProps {
 
 const StoryItem: React.FC<StoryItemProps> = ({ profilePic, onClick }) => {
   return (
-    <div className="profile-picture" onClick={onClick}>
-      <img src={profilePic} alt="User profile picture" />
+    <div>
+      <div className="profile-picture" onClick={onClick}>
+        <img src={profilePic} alt="User profile picture" />
+      </div>
+      <div className="profile-user-name">Ram Krishna</div>
     </div>
   );
 };
