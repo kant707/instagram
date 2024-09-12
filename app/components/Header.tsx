8bbf584952fc2c575368e276a8be749df9e55c67
Shashikant <kant707@gmail.com>
2024-09-13 02:11:34 +0530

    logo wrap in anchor tag and remove close button hover class

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
     <header className="main-header">
-      <img
-        className="brand-logo"
-        src="/icons/instagram_logo.svg"
-        height={40}
-        width={112}
-        alt="instagram logo"
-      />
+      <Link href={"/"} className="flex">
+        <img
+          className="brand-logo"
+          src="/icons/instagram_logo.svg"
+          height={40}
+          width={112}
+          alt="instagram logo"
+        />
+      </Link>
     </header>
   );
 };
