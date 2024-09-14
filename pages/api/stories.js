e5ce3b45d86bccc3b942ab557d4005659462d9d6
Shashikant <kant707@gmail.com>
2024-09-14 17:25:35 +0530

    env url

--- pages/api/stories.js
+++ pages/api/stories.js
@@ -1,10 +1,25 @@
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
+
+// export default async function handler(req, res) {
+//   const storiesPath = path.join(process.cwd(), "public", "stories.json");
+//   const storiesData = await fs.promises.readFile(storiesPath, "utf8");
+//   const stories = JSON.parse(storiesData);
+
+//   res.status(200).json(stories);
+// }
 
 export default async function handler(req, res) {
-  const storiesPath = path.join(process.cwd(), "public", "stories.json");
-  const storiesData = await fs.promises.readFile(storiesPath, "utf8");
-  const stories = JSON.parse(storiesData);
+  const storiesUrl = `${
+    process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+  }/stories.json`;
 
-  res.status(200).json(stories);
+  try {
+    const response = await fetch(storiesUrl);
+    const stories = await response.json();
+
+    res.status(200).json(stories);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to load stories" });
+  }
 }
