b27a215022d010a00f58b0e54bc00c89792c76d3
Shashikant <kant707@gmail.com>
2024-09-14 19:24:23 +0530

    push

--- pages/api/stories.js
+++ pages/api/stories.js
@@ -20,6 +20,10 @@ export default async function handler(req, res) {
 
     res.status(200).json(stories);
   } catch (error) {
-    res.status(500).json({ error: "Failed to load stories" });
+    res.status(500).json({
+      error: "Failed to load stories",
+      errorObj: error,
+      storiesUrl: storiesUrl,
+    });
   }
 }
