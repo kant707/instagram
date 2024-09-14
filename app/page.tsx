d86d5186fede2cc84082787e84a8a9b205d3c1ea
Shashikant <kant707@gmail.com>
2024-09-14 17:05:07 +0530

    url setup

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import UserItem from "./components/UserItem";
 import StoryPlayer from "./components/StoryPlayer";
 import Header from "./components/Header";
+import { BASE_API_URL } from "./utils/constant";
 
 export interface Story {
   storySrc: string;
@@ -29,7 +30,7 @@ const Home = () => {
   const getData = async () => {
     try {
       const apiUrl =
-        process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/stories";
+        `${BASE_API_URL}/api/stories` || "http://localhost:3000/api/stories";
       const response = await fetch(apiUrl);
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
